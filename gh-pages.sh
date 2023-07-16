a07c78d1ce8353f0af2ffe82023ecdbfe96dfab1
Humberto Dias <hldtux@gmail.com>
2023-07-16 16:27:15 -0300

    disable build.yml

--- gh-pages.sh
+++ gh-pages.sh
@@ -5,6 +5,6 @@ TMP_DIR="/tmp/pwa-$(date +'%Y_%m_%d %H:%M:%S')"
 cp -r dist/pwa "$TMP_DIR" && \
 git checkout gh-pages && \
 rm -rf * && \
-cp -r $TMP_DIR/* . && \
+cp -r "$TMP_DIR"/* . && \
 git commit . -m "gh-pages.sh" && \
 git push
