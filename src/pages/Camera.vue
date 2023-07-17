1baaad43cd1619bb96992a2cc14597a7fe01e3b6
Humberto Dias <hldtux@gmail.com>
2023-07-16 21:56:02 -0300

    checkPermission

--- src/pages/Camera.vue
+++ src/pages/Camera.vue
@@ -44,8 +44,61 @@ export default {
     if (navigator.mediaDevices.getUserMedia) {
       this.enableCamera = true;
     }
+    this.enumerateDevices();
+    this.checkPermission();
+    this.notifyMe();
   },
   methods: {
+    // https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/enumerateDevices
+    enumerateDevices() {
+      if (!navigator.mediaDevices?.enumerateDevices) {
+        console.log("enumerateDevices() not supported.");
+      } else {
+        // List cameras and microphones.
+        navigator.mediaDevices
+          .enumerateDevices()
+          .then((devices) => {
+            devices.forEach((device) => {
+              console.log(
+                `${device.kind}: ${device.label} id = ${device.deviceId}`
+              );
+            });
+          })
+          .catch((err) => {
+            console.error(`${err.name}: ${err.message}`);
+          });
+      }
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/API/Navigator/permissions
+    checkPermission() {
+      navigator.permissions.query({ name: "camera" }).then((result) => {
+        console.log("permission", result);
+      });
+    },
+    // https://developer.mozilla.org/en-US/docs/Web/API/Notification/permission_static
+    notifyMe() {
+      if (!("Notification" in window)) {
+        // Check if the browser supports notifications
+        alert("This browser does not support desktop notification");
+      } else if (Notification.permission === "granted") {
+        // Check whether notification permissions have already been granted;
+        // if so, create a notification
+        const notification = new Notification("Hi there!");
+        // …
+      } else if (Notification.permission !== "denied") {
+        // We need to ask the user for permission
+        Notification.requestPermission().then((permission) => {
+          // If the user accepts, let's create a notification
+          if (permission === "granted") {
+            const notification = new Notification("Hi there!");
+            // …
+          }
+        });
+      }
+
+      // At last, if the user has denied notifications, and you
+      // want to be respectful there is no need to bother them anymore.
+    },
     useCamera() {
       navigator.mediaDevices
         .getUserMedia({ video: true })
