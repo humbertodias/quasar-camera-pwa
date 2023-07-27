f7d8efc5c512c5ccf9fc1586dd230be1e56a855f
Humberto Dias <hldtux@gmail.com>
2023-07-27 17:00:54 -0300

    ci

--- script/deploy-apk.sh
+++ script/deploy-apk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 QENV="${1:-local}"
-docker run -v $(PWD)/..:/tmp/app -w /tmp/app hldtux/quasar-apk bash build-apk.sh $QENV
+docker run -v $(pwd)/..:/tmp/app -w /tmp/app hldtux/quasar-apk bash build-apk.sh $QENV
 
