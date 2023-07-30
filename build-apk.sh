0af69a602fae73c6f5489a018c0bf749f6730e33
Humberto Dias <hldtux@gmail.com>
2023-07-29 23:59:01 -0300

    removing plugin

--- build-apk.sh
+++ build-apk.sh
@@ -4,8 +4,4 @@ QENV=${1:-local}
 docker run -e QENV=local \
            -v $(pwd):/tmp/quasar-workdir \
            -w /tmp/quasar-workdir \
-           hldtux/quasar-apk bash -c "npm install && \
-                                      quasar build -m cordova -T android --debug && \
-                                      cd src-cordova && \
-                                      cordova plugin add https://github.com/humbertodias/cordova-plugin-sendkeystrokes.git && \
-                                      cordova build android"
+           hldtux/quasar-apk bash -c "npm install && quasar build -m cordova -T android --debug"
