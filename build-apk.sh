172a18e3405f440543282a844b6455e09ffdc60e
Humberto Dias <hldtux@gmail.com>
2023-07-30 13:18:45 -0300

    ci

--- build-apk.sh
+++ build-apk.sh
@@ -4,6 +4,8 @@ QENV=${1:-local}
 echo "QENV=$QENV"
 docker run -e QENV=$QENV \
            -v $(pwd):/home/quasar/workdir \
+           -v /etc/localtime:/etc/localtime:ro \
+           -v /etc/hosts:/etc/hosts:ro \
            -w /home/quasar/workdir \
            --rm \
-           hldtux/quasar-apk bash -c "npm install && quasar build -m cordova -T android --debug"
+           hldtux/quasar-apk bash -c "npm install ; quasar build -m cordova -T android --debug ; cd src-cordova && npm i && cordova build android"
