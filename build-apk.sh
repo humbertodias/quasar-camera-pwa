04e25deb1072b21c7bbd5619673516db45b87709
Humberto Dias <hldtux@gmail.com>
2023-07-30 09:34:28 -0300

    ci

--- build-apk.sh
+++ build-apk.sh
@@ -4,4 +4,4 @@ QENV=${1:-local}
 docker run -e QENV=local \
            -v $(pwd):/tmp/quasar-workdir \
            -w /tmp/quasar-workdir \
-           hldtux/quasar-apk bash -c "npm install && quasar build -m cordova -T android --debug"
+           hldtux/quasar-apk bash -c "git pull --recurse-submodules && ls -lha src-cordova && npm install && quasar build -m cordova -T android --debug"
