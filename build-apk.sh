1e3cf5ad2c3e772577ae78086b1f76df334a8f34
Humberto Dias <hldtux@gmail.com>
2023-07-29 23:33:19 -0300

    ci

--- build-apk.sh
+++ build-apk.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+QENV=${1:-local}
 docker run -e QENV=local \
-          -v $(pwd):/tmp/quasar-workdir \
-          -w /tmp/quasar-workdir \
-          hldtux/quasar-apk bash -c "npm install && \
-                                     quasar build -m cordova -T android --debug && \
-                                     cd src-cordova && \
-                                     cordova plugin add https://github.com/keilyn3d/cordova-plugin-sendkeystrokes.git && \
-                                     cordova build android"
+           -v $(pwd):/tmp/quasar-workdir \
+           -w /tmp/quasar-workdir \
+           hldtux/quasar-apk bash -c "npm install && \
+                                      quasar build -m cordova -T android --debug && \
+                                      cd src-cordova && \
+                                      cordova plugin add https://github.com/keilyn3d/cordova-plugin-sendkeystrokes.git && \
+                                      cordova build android"
