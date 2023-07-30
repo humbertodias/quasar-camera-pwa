bfb3ca0870c2423df22b5f46706e3661bb49920e
Humberto Dias <hldtux@gmail.com>
2023-07-29 23:18:45 -0300

    ci

--- build-apk.sh
+++ build-apk.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
+
 npm install && \
-quasar build -m cordova -T android && \
+quasar build -m cordova -T android --debug && \
 
 cd src-cordova && \
 cordova plugin add https://github.com/keilyn3d/cordova-plugin-sendkeystrokes.git && \
-cordova build android && \
-
-find . -name '*.apk'
+cordova build android
