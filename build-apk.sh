64f3bac28fce2eabf3235f1eb46a2a66880ada05
Humberto Dias <hldtux@gmail.com>
2023-08-01 21:42:53 -0300

    java 11

--- build-apk.sh
+++ build-apk.sh
@@ -3,6 +3,10 @@
 QENV=${1:-local}
 echo "QENV=$QENV"
 docker run -e QENV=$QENV \
+           -e ANDROID_VERSION=33 \
+           -e ANDROID_BUILD_TOOLS_VERSION=33.0.2 \
+           -e JAVA_VERSION=11.0.20-zulu \
+           -e GRADLE_VERSION=7.6 \
            -v $(pwd):/home/quasar/workdir \
            -v /etc/localtime:/etc/localtime:ro \
            -v /etc/hosts:/etc/hosts:ro \
